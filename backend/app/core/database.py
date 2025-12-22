257c99a9a5a68e9ae7e04c8d4ae5408fef22ea4e
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2025-12-22 20:17:02 +0530

    Use DATABASE_URL from environment variables
    Replaced hardcoded DATABASE_URL with environment variable.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -11,10 +11,12 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = (
-  F"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-  f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# DATABASE_URL = (
+#   F"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+#   f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# )
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine (
   DATABASE_URL,
